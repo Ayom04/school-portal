5111f78aeac367ecc28667848454e8e331fc983d
Ayom04 <abdullahayomide04@gmail.com>
2024-04-02 22:34:39 +0100

    feat(generarate random character added some field to the db

--- src/routes/teacher.ts
+++
@@ -1,7 +0,0 @@
-import { v4 as uuidv4 } from "uuid";
-import jwt from "jsonwebtoken";
-import { NextFunction, Request, Response } from "express";
-import { hashPassword, comparePassword, generateOtp } from "../utils/helper";
-import response from "../utils/response";
-const models = require("../models");
-import messages from "../constants/messages";
